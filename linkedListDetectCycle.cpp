adfb90436daa187e10e47a2cf6e596d6624650fb
phani92 <phanindra.yellapu@gmail.com>
2021-05-27 23:06:48 +0200

    FloydsTortoiseHAreAlgo

---
+++ linkedListDetectCycle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *next;
+};
+
+// The fast and slow algorithm is called as Floyds tortoise and hare algorithm.
+// one counter runs at a double pace and the other at a slower pace.
+Node *next(Node *node)
+{
+  if (node)
+    node = node->next;
+  return node;
+}
+
+bool has_cycle(Node *head)
+{
+  Node *slow = next(head);
+  Node *fast = next(next(head));
+
+  while (slow != fast)
+  {
+    slow = next(slow);
+    fast = next(next(fast));
+  }
+
+  return fast;
+}
